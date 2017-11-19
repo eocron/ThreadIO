50b4b4fa6005162fc0e2a22f9ec36b1c89bc738b
eocron <ad-company@mail.ru>
2017-11-19 23:56:28 +0300

    More description

--- ThreadIO/FluentExtensions.cs
+++ ThreadIO/FluentExtensions.cs
@@ -5,6 +5,13 @@ namespace ThreadIO
 {
     public static class FluentExtensions
     {
+        /// <summary>
+        /// Adds key to fluent builder for write lock
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static LockScopeBuilder<TKey> Write<TKey>(this LockScopeBuilder<TKey> builder, params TKey[] args)
         {
             if (args == null)
@@ -13,6 +20,13 @@ namespace ThreadIO
             return builder;
         }
 
+        /// <summary>
+        /// Adds key to fluent builder for write lock
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static LockScopeBuilder<TKey> Write<TKey>(this LockScopeBuilder<TKey> builder, IEnumerable<TKey> args)
         {
             if (args == null)
@@ -21,6 +35,13 @@ namespace ThreadIO
             return builder;
         }
 
+        /// <summary>
+        /// Adds key to fluent builder for read lock
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static LockScopeBuilder<TKey> Read<TKey>(this LockScopeBuilder<TKey> builder, params TKey[] args)
         {
             if (args == null)
@@ -29,6 +50,13 @@ namespace ThreadIO
             return builder;
         }
 
+        /// <summary>
+        /// Adds key to fluent builder for read lock
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static LockScopeBuilder<TKey> Read<TKey>(this LockScopeBuilder<TKey> builder, IEnumerable<TKey> args)
         {
             if (args == null)
@@ -37,6 +65,13 @@ namespace ThreadIO
             return builder;
         }
 
+        /// <summary>
+        /// Adds timeout for lock operation
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
         public static LockScopeBuilder<TKey> Timeout<TKey>(this LockScopeBuilder<TKey> builder, TimeSpan timeout)
         {
             builder.WithTimeout = timeout;
